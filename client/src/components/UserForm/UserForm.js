252594a0de25a0bfca9ec6dd404e1fc6e41bf8ea
Chia Yeng Xiong <chiayengxiong@Chias-MacBook-Pro.local>
2020-12-10 21:18:47 -0600

    added component files.

---
+++ client/src/components/UserForm/UserForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignUp, PictureProfile, Welcome } from "../FormProcess/";
+
+export default function UserForm() {
+  return (
+    <div>
+      <SignUp />
+      <PictureProfile />
+      <Welcome />
+    </div>
+  );
+}
